803173105083cc60076587fc2629e7bb33ea071a
Sourabh <sourabhpatole@outlook.com>
2026-01-21 19:52:16 +0530

    changes in app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.use((redq, res) => {
+app.use((red, res) => {
   res.send("Hello from the Node js server!!");
 });
-
+app.use("/hello", (req, res) => {
+  res.send("Hello Hello , Hello!");
+});
+app.use("/test", (req, res) => {
+  res.send("Hello from the server");
+});
 app.listen(5000, () => {
   console.log("Server is running on the port 5000");
 });
