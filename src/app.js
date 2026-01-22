0093369891ccd9b6b54a3285977f80573be86b3e
Sourabh <sourabhpatole@outlook.com>
2026-01-22 16:05:01 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const app = express();
 
-// this will only handle get call from user
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Sourabh", lastName: "Patole" });
-});
-app.post("/user", (req, res) => {
-  console.log("Save data to the database");
-  res.send("Data successfully saved to the document");
-});
+// app.get("/abc", (req, res) => {
+//   res.send({ firstName: "Sourabh", lastName: "Patole" });
+// });
+
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling the route user 1!!");
+    // res.send("Response!!");
+    next();
+  },
+  (req, res) => {
+    console.log("Handling the route user 2!!");
+    res.send("2nd Response!!");
+  },
+);
+
 app.listen(5000, () => {
   console.log("Server is running on the port 5000");
 });
