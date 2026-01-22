d034724ef6934c87a31564fe46863977fdea96d1
Sourabh <sourabhpatole@outlook.com>
2026-01-22 13:45:50 +0530

    created method for get and post

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.use("/", (red, res) => {
-  res.send("Hello from the Node js server!!");
+// this will only handle get call from user
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Sourabh", lastName: "Patole" });
 });
-app.use("/hello", (req, res) => {
-  res.send("Hello Hello , Hello!");
-});
-app.use("/test", (req, res) => {
-  res.send("Hello from the server");
+app.post("/user", (req, res) => {
+  console.log("Save data to the database");
+  res.send("Data successfully saved to the document");
 });
 app.listen(5000, () => {
   console.log("Server is running on the port 5000");
