41e66d384d9f7bc03941eac6b4bbf6fc87c15bd6
Sourabh <sourabhpatole@outlook.com>
2026-01-22 17:17:33 +0530

    middleware

--- src/app.js
+++ src/app.js
@@ -1,23 +1,11 @@
 const express = require("express");
 const app = express();
-
-// app.get("/abc", (req, res) => {
-//   res.send({ firstName: "Sourabh", lastName: "Patole" });
-// });
-
-app.use(
-  "/user",
-  (req, res, next) => {
-    console.log("Handling the route user 1!!");
-    // res.send("Response!!");
-    next();
-  },
-  (req, res) => {
-    console.log("Handling the route user 2!!");
-    res.send("2nd Response!!");
-  },
-);
-
+app.get("admin/getAllData", (req, res) => {
+  res.send("All data sent");
+});
+app.get("admin/deleteUser", (req, res) => {
+  res.send("Deleted the user");
+});
 app.listen(5000, () => {
   console.log("Server is running on the port 5000");
 });
