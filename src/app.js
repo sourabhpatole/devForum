d95d81685ab66bcc84db3cdd0e8befdee3dee6eb
Sourabh <sourabhpatole@outlook.com>
2026-01-31 12:43:23 +0530

    delete api

--- src/app.js
+++ src/app.js
@@ -47,12 +47,17 @@ app.get("/feed", async (req, res) => {
   }
 });
 app.delete("/user", async (req, res) => {
-  const userId = req.body.userId;
+  let userId = req.body.userId;
   try {
-    const user = await User.findByIdAndDelete(userId);
-    res.status(200).send("User deleted successfully");
+    let user = await User.findByIdAndDelete(userId);
+    if (!user) {
+      res.status(401).send("User not found");
+      return;
+    } else {
+      res.status(200).send("User successfully deleted");
+    }
   } catch (error) {
-    res.status(400).send("Something went wrong");
+    res.status(400).send("Something went wrong!");
   }
 });
 
